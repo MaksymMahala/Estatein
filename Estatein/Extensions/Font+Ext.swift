1abffb6ec51bb66eeae42067de9ed7698d569bf5
maksym.mahala <maksym.mahala@intenselab.com>
2024-09-30 19:20:05 +0300

    Added custonTabBar, created Constants and size adaptivating: scaled(). Created HomeView and HomeViewModel. Made Main View and MainViewModel

---
+++ Estatein/Extensions/Font+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  Font+Ext.swift
+//  Estatein
+//
+//  Created by Max on 30.09.2024.
+//
+
+import Foundation
+import SwiftUICore
+
+extension Font {
+    static var soraBold20 = Font.custom("Sora-Bold", fixedSize: 20)
+    
+    static var orbitronRegular20 = Font.custom("Orbitron-Regular", fixedSize: 20)
+    static var orbitronRegular14 = Font.custom("Orbitron-Regular", fixedSize: 14)
+    static var orbitronRegular12 = Font.custom("Orbitron-Regular", fixedSize: 12)
+    static var orbitronRegular8 = Font.custom("Orbitron-Regular", fixedSize: 8)
+
+    static var orbitronBold12 = Font.custom("Orbitron-Bold", fixedSize: 12)
+
+}
---
+++ Estatein/UI/Main/MainView.swift
@@ -0,0 +1,51 @@
+//
+//  MainView.swift
+//  Estatein
+//
+//  Created by Max on 30.09.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @StateObject private var viewModel = MainViewModel()
+    
+    var body: some View {
+        ZStack {
+            switch viewModel.selectedTab {
+            case "Home":
+                HomeView()
+            case "Markets":
+                ContentView()
+            case "Trades":
+                ContentView()
+            case "Activity":
+                ContentView()
+            case "Wallets":
+                ContentView ()
+            default:
+                HomeView()
+            }
+            
+            VStack {
+                Spacer()
+                CustomTabBarView(selectedTab: $viewModel.selectedTab)
+            }
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MainView()
+                .previewDevice("iPhone 16 Pro")
+                .previewDisplayName("iPhone 16 Pro")
+            
+            MainView()
+                .previewDevice("iPhone SE")
+                .previewDisplayName("iPhone SE")
+        }
+    }
+}
